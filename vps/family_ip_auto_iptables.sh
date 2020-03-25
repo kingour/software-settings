b3d7a77b19dd31a895a4f6b4744afba7fc02d8c4
Kingour <kingour1986@gmail.com>
2020-03-25 18:04:53 +0800

    bugfix for iptables

--- vps/family_ip_auto_iptables.sh
+++ vps/family_ip_auto_iptables.sh
@@ -5,6 +5,7 @@ wd=$(cd `dirname ${0}` && pwd)
 . ${wd}/../lib/*.sh
 
 . /root/.bashrc
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:$PATH
 
 if [[ -z "${FAMILY_DDNS_HOST}" ]];then
     log "Pls set FAMILY_DDNS_HOST env param, like 'export FAMILY_DDNS_HOST=a.com'"
@@ -18,10 +19,15 @@ newip=$(nslookup "${FAMILY_DDNS_HOST}" | grep -A 1 "${FAMILY_DDNS_HOST}" | grep 
 oldip=$(/sbin/iptables -nvL ${USER_WHITELIST} 1 | awk '{print $8}')
 log "[HOST: ${FAMILY_DDNS_HOST}][oldip:${oldip}, newip:${newip}]"
 
+chain_flag=$(/sbin/iptables -S ${USER_WHITELIST} | grep "\-N" | wc -l)
+if [[ ${chain_flag} -eq 0 ]];then
+    /sbin/iptables -N ${USER_WHITELIST}
+fi
+
 if [[ x"${newip}" != x"${oldip}" ]];then
     log "replace first ip"
     mkdir -p /root/dev/iptables
     /bin/cp -f /etc/sysconfig/iptables /root/dev/iptables/iptables.$(date +%Y%m%d)
-    iptables -R ${USER_WHITELIST} 1 -s ${newip} -j ACCEPT
-    service iptables save
+    /sbin/iptables -R ${USER_WHITELIST} 1 -s ${newip} -j ACCEPT
+    /usr/sbin/service iptables save
 fi
